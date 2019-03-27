f144b887bc64c70d732ab44b4a0d36300386b28b
Dan <danielpowell12@msn.com>
2019-03-27 11:21:58 -0600

    Prototypes widget w/ fetch

---
+++ src/components/MarketProfiles/index.js
@@ -0,0 +1,65 @@
+import { h, Component } from "preact";
+import fetch from "cross-fetch";
+import style from "./style";
+
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const baseUrl = "https://staging.privateprep.com/feeds/profiles?roles=director";
+const localUrl = "http://localhost:5000/feeds/profiles?roles=director";
+
+const requestHeaders = {
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+};
+
+export default class App extends Component {
+  state = {
+    profiles: [],
+    isLoading: false,
+  };
+
+  componentDidMount() {
+    this.fetchProfiles();
+  }
+
+  fetchProfiles = () => {
+    this.setState({ isLoading: true });
+
+    fetch(`${localUrl}&location=${this.props.location}`, requestHeaders)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(({ profiles }) => {
+        this.setState({
+          isLoading: false,
+          profiles: profiles.sort(({ position: a }, { position: b }) =>
+            a < b ? -1 : a > b ? 1 : 0
+          ),
+        });
+      })
+      .catch(error => {
+        console.error("Error occurred while loading profiles", error);
+      });
+  };
+
+  render(props) {
+    return (
+      <div class={style.profilesContainer}>
+        {this.state.isLoading && <p>Loading...</p>}
+        {this.state.profiles.map((profile, i) => (
+          <div class={style.profileCard} key={i}>
+            <img
+              src={profile.picture_url}
+              alt={`${profile.name} profile photo`}
+            />
+            <h4>{profile.name}</h4>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ let poly = require("preact-cli/lib/lib/webpack/polyfills");
 import { h } from "preact";
 import habitat from "preact-habitat";
 
-import Widget from "./components/hello-world";
+import Widget from "./components/MarketProfiles";
 
 let _habitat = habitat(Widget);
 
 _habitat.render({
   selector: '[data-widget-host="habitat"]',
-  clean: true
+  clean: true,
 });
